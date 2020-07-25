7369ef8d7e9e28d5af636da1323e437922afc89d
Sunil <sunilsv26@gmail.com>
2020-07-25 12:09:44 +0530

    crtd sngle prdctItm and log of addCrtBtn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,42 +1,75 @@
-const productList = {
-    products:[
-        {
-            title:'TCL 4k UHD',
-            imageUrl:`https://assets.products-live.ao.com/Images/e9f3edf8-056f-425a-9b17-06a09c6ca869/1280x1280/55dp628_tcl_ledtv_01c.jpg`,
-            Price: 'Rs.24000',
-            spec:'You will love it',
-        },
-        {
-            title:'TCL 4k UHD',
-            imageUrl:`https://assets.products-live.ao.com/Images/e9f3edf8-056f-425a-9b17-06a09c6ca869/1280x1280/55dp628_tcl_ledtv_01c.jpg`,
-            Price: 'Rs.24000',
-            spec: 'True inspiration'
-        },
-    ],
+class Product {
+  /* title;
+    imageUrl;
+    Price;
+    spec; */
 
-    render(){
-        const prodDom = document.getElementById('app');
-        const prodList = document.createElement('ul');
-        prodList.className = 'product-list';
-        for(const prod of this.products){
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
+  constructor(title, img, prce, dscrptn) {
+    this.title = title;
+    this.imageUrl = img;
+    this.Price = prce;
+    this.spec = dscrptn;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart(){
+      console.log('Adding product to cart');
+      console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
             <div>
-              <img src="${prod.imageUrl} alt ="${prod.title}"></img>
+              <img src="${this.product.imageUrl} alt ="${this.product.title}"></img>
               <div class="product-item__content">
-              <h2>${prod.title}</h2>
-              <h3>${prod.Price}</h3>
-              <p>${prod.spec}</p>
+              <h2>${this.product.title}</h2>
+              <h3>${this.product.Price}</h3>
+              <p>${this.product.spec}</p>
               <button>Add to Cart</button>
               </div>
             </div> 
-            `
-            ;
-            prodList.append(prodEl)
-        };
-        prodDom.append(prodList)
-    }
+            `;
+    const addToCartBtn = prodEl.querySelector('button');
+    addToCartBtn.addEventListener('click',this.addToCart.bind(this))
+    return prodEl;
+    
+  }
 }
 
+class ProductList {
+  products = [
+    new Product(
+      "TCL 4k UHD",
+      `https://assets.products-live.ao.com/Images/e9f3edf8-056f-425a-9b17-06a09c6ca869/1280x1280/55dp628_tcl_ledtv_01c.jpg`,
+      "Rs.24000",
+      "You will love it"
+    ),
+    new Product(
+      "TCL 4k UHD",
+      `https://assets.products-live.ao.com/Images/e9f3edf8-056f-425a-9b17-06a09c6ca869/1280x1280/55dp628_tcl_ledtv_01c.jpg`,
+      "Rs.24000",
+      "True inspiration"
+    ),
+  ];
+  render() {
+    const prodDom = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+    }
+    prodDom.append(prodList);
+  }
+}
+
+const productList = new ProductList();
 productList.render();
