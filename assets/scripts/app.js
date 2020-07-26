532f3b5049cdb7f8341f76de4ac7579ff3c8bcc6
Sunil <sunilsv26@gmail.com>
2020-07-26 13:59:29 +0530

    use of inheritance,get etc

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,4 @@
 class Product {
-  /* title;
-    imageUrl;
-    Price;
-    spec; */
-
   constructor(title, img, prce, dscrptn) {
     this.title = title;
     this.imageUrl = img;
@@ -11,20 +6,48 @@ class Product {
     this.spec = dscrptn;
   }
 }
-
-class ProductItem {
-  constructor(product) {
-    this.product = product;
+class Component{
+  constructor(prodDom){
+    this.prodDom = prodDom;
   }
 
+  createRootElement(tag,cssClasses,attributes){
+    const rootElement = document.createElement(tag);
+    if(cssClasses){
+      rootElement.className = cssClasses;
+    }
+    if(attributes && attributes.length>0){
+      for (const attr of attributes){
+        rootElement.setAttribute(attr.name,attr.value);
+      }
+    }
+    document.getElementById(this.prodDom).append(rootElement);
+    return rootElement; 
+  }
+  
+}
+
+class ElementAttribute{
+  constructor(attrName,attrValue){
+    this.name = attrName;
+    this.value=attrValue
+  }
+}
+
+
+class ProductItem extends Component {
+  constructor(product,prodDom) {
+    super(prodDom)
+    this.product = product; 
+  }
+
+
   addToCart(){
-      console.log('Adding product to cart');
-      console.log(this.product);
+      App.addProductToCart(this.product)
   }
 
   render() {
-    const prodEl = document.createElement("li");
-    prodEl.className = "product-item";
+    const prodEl = this.createRootElement('li','product-item') ;
     prodEl.innerHTML = `
             <div>
               <img src="${this.product.imageUrl} alt ="${this.product.title}"></img>
@@ -38,67 +61,102 @@ class ProductItem {
             `;
     const addToCartBtn = prodEl.querySelector('button');
     addToCartBtn.addEventListener('click',this.addToCart.bind(this))
-    return prodEl;
     
   }
 }
 
-class ShoppingCart {
-    Item= [];
+
+
+class ShoppingCart extends Component{
+    items= [];
+
+    get totalSum(){
+        const sum = this.items.reduce((preVal,curItem)=>{
+            return preVal + curItem.Price;
+        },0);
+        return sum;
+        
+    } 
+    
+    constructor(prodDom){
+      super(prodDom);
+    }
+
+    addProduct (product){
+        this.items.push(product); 
+        this.totalOutput.innerHTML = `<h2>Total: ${this.totalSum}</h2>`
+    }
+
+    orderNow(){
+      console.log(this.items)
+
+    }
 
     render(){
-        const cartEl = document.createElement('section');
+        const cartEl=this.createRootElement('section','cart');
         cartEl.innerHTML =`
-        <h2>Total: ${0}</h2>
+        <h2>Total: ${this.totalSum}</h2>
         <button>Order Now</button>
         `;
-        cartEl.className = 'cart';
+        const orderBtn = cartEl.querySelector('button');
+        orderBtn.addEventListener('click',this.orderNow.bind(this))
+        this.totalOutput = cartEl.querySelector('h2')
         return cartEl;
     }
 }
 
-class ProductList {
+class ProductList extends Component{
   products = [
     new Product(
       "TCL 4k UHD",
       `https://assets.products-live.ao.com/Images/e9f3edf8-056f-425a-9b17-06a09c6ca869/1280x1280/55dp628_tcl_ledtv_01c.jpg`,
-      "Rs.24000",
+      24000,
       "You will love it"
     ),
     new Product(
       "TCL 4k UHD",
       `https://assets.products-live.ao.com/Images/e9f3edf8-056f-425a-9b17-06a09c6ca869/1280x1280/55dp628_tcl_ledtv_01c.jpg`,
-      "Rs.24000",
+      48000,
       "True inspiration"
     ),
   ];
+  
   render() {
-    const prodList = document.createElement("ul");
-    prodList.className = "product-list";
+    const prodList = this.createRootElement('ul','product-list',[new ElementAttribute('id','prod-list')])
     for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
+      const productItem = new ProductItem(prod,'prod-list');
+      productItem.render();
     }
-    return prodList;
+
+  }
+  constructor(prodDom){
+    super(prodDom)
   }
 }
 
 class Shop {
     render(){
-        const prodDom = document.getElementById("app");
-        const cart = new ShoppingCart();
-        const cartEl = cart.render();
-        const productList = new ProductList();
-        const productListEl =productList.render();
 
-        prodDom.append(cartEl);
-        prodDom.append(productListEl);
-        
-
+        this.cart = new ShoppingCart('app');
+        this.cart.render();
+        const productList = new ProductList('app');
+        productList.render();
     }
 }
 
-const shop =new Shop();
-shop.render()
+class App {
+    static init(){
+        const shop =new Shop();
+        shop.render()
+        this.cart = shop.cart
+        
+    }
+ 
+    static addProductToCart (product){
+        this.cart.addProduct(product)
+    }
+}
+
+App.init();
+ 
 
